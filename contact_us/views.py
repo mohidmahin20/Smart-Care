4c12094cd0a71ea544143f3c551c75a6bb7a1643
Mahin <email@example.com>
2025-02-10 20:10:40 +0600

    smart care api

---
+++ contact_us/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+# Create your views here.
+from . import models
+from . import serializers
+class ContactUsViewset(viewsets.ModelViewSet):
+    queryset =  models.ContactUs.objects.all()
+    serializer_class = serializers.ContactUsSerializer
